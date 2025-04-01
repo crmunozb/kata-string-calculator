f4096216085a1e31a9483329501eaa9f77795bb2
Cristobal Muñoz Barrios <crmunoz2018@udec.cl>
2025-04-01 00:02:52 -0300

    refactor: implementar suma real de múltiples números

--- string_calculator.py
+++ string_calculator.py
@@ -1,5 +1,14 @@
 def sumar(cadena):
+    # Si la cadena está vacía, no hay números para sumar y retorna 0
     if cadena == "":
-        return 0 #Si la cadena inicial es vacia 
-    return 3 #Valor fijo para el primer test donde se suma 1 y 2
-
+        return 0
+    
+    # Usamos split(",") para separar 
+    # Ej: "4,5,6" seria ['4', '5', '6']
+    partes = cadena.split(",")
+    
+    # Usamos map(int, ...) para convertir cada parte a entero
+    # Ej: ['4', '5', '6'] -> [4, 5, 6]
+    numeros = map(int, partes)
+    # Sumamos todos los números y retornamos el resultado
+    return sum(numeros)
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -10,6 +10,10 @@ class TestStringCalculator(unittest.TestCase): #Clase que contiene los test para
         resultado = sumar("")
         self.assertEqual(resultado, 0) #Verificamos que el resultado es 0 , ya que no existen numeros para sumar
 
+    def test_multiples_numeros(self):
+        resultado = sumar ("4,5,6") # Debe sumar los numeros 4,5 y 6 
+        self.assertEqual(resultado, 15) #Verificamos que el valor de la suma sea 15
+
 
 if __name__ == '__main__': # Permite correr los test directamente al ejecutar este archivo
     unittest.main()
