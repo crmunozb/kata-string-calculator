c156171f0a9706ab466abbfcf1fd949044754153
Cristobal Muñoz Barrios <crmunoz2018@udec.cl>
2025-04-01 21:55:10 -0300

    test: delimitador personalizado funcionando 💪

--- string_calculator.py
+++ string_calculator.py
@@ -2,12 +2,23 @@ def sumar(cadena):
     # Si no hay nada en la cadena, retornamos 0
     if cadena == "":
         return 0
+    
+    #por defecto usaremos la coma como separador
+
+    delimitador = ","
+    #Si parte con // usaremos un delimitador personalizado
+
+    if cadena.startswith("//"):
+        partes_delim = cadena.split("\n", 1)  # Separamos delimitador del resto
+        delimitador = partes_delim[0][2:]     # Tomamos solo el delimitador
+        cadena = partes_delim[1]              # Ahora cadena solo tiene los números
+
 
     # Cambiamos los saltos de línea por comas
-    cadena = cadena.replace("\n", ",")
+    cadena = cadena.replace("\n", delimitador)
 
-    # Partimos la cadena usando la coma como separador
-    partes = cadena.split(",")
+    # Partimos la cadena usando el delimitador que se este usando
+    partes = cadena.split( delimitador)
 
     # Convertimos todas las partes a enteros
     numeros = list(map(int, partes))
